4a01f742d8824f157e470d5ea05ca0a8e4077a5e
Jiawen Xu <jix034@ucsd.edu>
2021-10-22 11:13:19 -0700

    initial commit

---
+++ expose.js
@@ -0,0 +1,11 @@
+// Display correct image after selecting a horn from the drop down menu
+function display_image(){
+    var expose = document.getElementById("expose");
+    var horn_select = document.getElementById("horn-select");
+    if(horn_select.value == "air-horn"){
+
+    }
+    else if(horn_select.value == ""){
+
+    }
+}
